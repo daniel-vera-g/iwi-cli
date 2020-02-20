1fde354aa810b5fadd7fbcb8932e56ae1abf377c
danielVg <danielveragi@gmail.com>
2020-02-20 21:46:29 +0100

    Final refactoring & tests for the mensa feature

---
+++ tests/test.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect;
+const mensa = require('../lib/mensa');
+
+describe('mensa.js', function () {
+    describe('getData()', function () {
+        it('should return an object with meals and prices.', function (done) {
+            mensa.getData().then(Meals => {
+                expect(Meals).to.be.an('object');
+                for (Meal in Meals) {
+                    expect(Meals[Meal][0]).to.be.a('string');
+                    expect(Meals[Meal][1]).to.be.a('number')
+                }
+                done();
+            }).catch(e => {
+                console.error(e);
+                done();
+            })
+        });
+    });
+});
