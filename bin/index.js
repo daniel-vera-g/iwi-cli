a80a90a6980cf731693834aed92c7d32d526a92e
danielVg <danielveragi@gmail.com>
2020-02-23 20:19:29 +0100

    Eslint formating

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const yargs = require("yargs");
+const yargs = require('yargs');
 
 // Describe parameters, examples, aliases,...
 const args = yargs
-    .usage("Usage: iwi-cli <command> [options]")
-    .command(require("../lib/mensa"))
-    .example("iwi-cli mensa", "Get the mensa plan for the current day")
-    .example("iwi-cli mensa --veg", "Get the mensa plan only including vegetarian dishes")
-    .demandCommand(1, "You need at least one command before moving on")
-    .help("h")
-    .alias("h", "help")
-    .epilogue("for more information, check out the official IWI web-app at https://iwi-i-info.firebaseapp.com/").argv;
+	.usage('Usage: iwi-cli <command> [options]')
+	.command(require('../lib/mensa'))
+	.example('iwi-cli mensa', 'Get the mensa plan for the current day')
+	.example('iwi-cli mensa --veg', 'Get the mensa plan only including vegetarian dishes')
+	.demandCommand(1, 'You need at least one command before moving on')
+	.help('h')
+	.alias('h', 'help')
+	.epilogue('for more information, check out the official IWI web-app at https://iwi-i-info.firebaseapp.com/').argv;
